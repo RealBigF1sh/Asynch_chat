3fd5d7b20c7bdf65dd56200a45386bb8ccf631d0
RBF <tarans.fishborn@gmail.com>
2023-05-29 04:35:32 +0300

    task 3 done

---
+++ task_3.py
@@ -0,0 +1,13 @@
+s_1 = b'attribute'
+s_2 = b'класс'
+s_3 = b'функция'
+s_4 = b'type'
+
+#    File "/home/rbf/Рабочий стол/RBF_Asynch_chat/Asynch_chat/task_3.py", line 2
+#         s_2 = b'класс'
+#            ^
+# SyntaxError: bytes can only contain ASCII literal characters.
+#   File "/home/rbf/Рабочий стол/RBF_Asynch_chat/Asynch_chat/task_3.py", line 3
+#     s_3 = b'функция'
+#            ^
+# SyntaxError: bytes can only contain ASCII literal characters.
